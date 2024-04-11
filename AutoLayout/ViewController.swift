f8bd5d7f25abf133bdc6497431aa1cfdce7354d6
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-11 10:32:01 -0300

    Added labels and VFL

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -11,7 +11,71 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false // TAMIC, which means we have to make our constraints by hand, auto resizing masks is an Old iOS way of doing layout.
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false // TAMIC
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false // TAMIC
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false // TAMIC
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false // TAMIC
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        
+        // VISUAL FORMAT LANGUAGE
+        
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        
+        for label in viewsDictionary.keys {
+            view // Main View
+                .addConstraints( // Adds an array of constraints to View
+                    NSLayoutConstraint.constraints( // Auto Layout method that converts VFL into an array of constraints
+                        withVisualFormat: "H:|[\(label)]|", //  H: means horizontal, | means edge, H:|[label1]| means "horizontally, I want my label1 to go edge to edge in my view."
+                        options: [.center],
+                        metrics: nil,
+                        views: viewsDictionary
+                    )
+                )
+        }
+        
+        view
+            .addConstraints(
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", // - symbol, which means "space". It's 10
+                    options: [],
+                    metrics: nil,
+                    views: viewsDictionary
+                )
+            )
+        
     }
 
 
